a8fae1dc5039c7dfbec581faabfede956f43c399
Michelle Olivieri <michelleo72@gmail.com>
2018-05-14 21:39:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
-    @post= Post.create(name: params[:name], content: params[:content])
+    #@post= Post.create(name: params[:name], content: params[:content])
+    @post = Post.create(params)
     redirect to '/posts'
   end
 
